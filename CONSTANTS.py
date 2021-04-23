2b3ab89ec7830f0300a745944fa6cb2ac6a5a198
Mike Zborowski <mzborowski@yahoo.com>
2021-04-23 19:47:02 -0400

    update

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -11,6 +11,10 @@ if os.name == 'nt':
     DEFAULT_DB_FILEPATH = 'C:\\Users\\MikeAdmin\\Documents\\Python\\Projects\\coinmaker\\var\\sqlite3db'
     DEFAULT_LOG_FILEPATH = 'C:\\Users\\MikeAdmin\\Documents\\Python\\Projects\\coinmaker\\logs\\coinmaker.log'
 elif os.name == 'posix':
+    DEFAULT_SECRETS_FILEPATH = '/home/mike/.dcabot_secrets'
+    DEFAULT_CONFIG_FILEPATH = '/home/mike/bin/coinmaker/coinmaker/etc/coinmaker.conf'
+    DEFAULT_DB_FILEPATH = '/home/mike/bin/coinmaker/coinmaker/var/sqlitedb'
+    DEFAULT_LOG_FILEPATH = '/home/mike/bin/coinmaker/coinmaker/logs/coinmaker.log'
     pass
 
 else:
