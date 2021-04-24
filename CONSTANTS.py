08b67b8a39794c91de1189be97243ccae75cef06
Zeebrow <mzborowski@yahoo.com>
2021-04-24 18:49:51 -0400

    update

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -1,4 +1,16 @@
-import os
+import os, sys
+from pathlib import Path
+import json
+import logging
+
+log = logging.getLogger(__name__)
+
+
+user = os.getlogin()
+log.debug(f"Running {json.dumps({'program': sys.argv[0], 'as_user': user})}")
+log.debug(f"OS type {os.name}")
+
+homedir = ['/home/'+os.getlogin()+'/' if os.name == 'posix' else '/Users/'+os.getlogin()+'/']
 
 DEFAULT_SECRETS_FILEPATH = ''
 DEFAULT_CONFIG_FILEPATH = ''
@@ -6,17 +18,30 @@ DEFAULT_DB_FILEPATH = ''
 DEFAULT_LOGS_FILEPATH = ''
 
 if os.name == 'nt':
+    #_default_secrets_filepath = homedir + str(Path('Documents/Python/Projects/coinmaker/secrets') )
+    #DEFAULT_SECRETS_FILEPATH = homedir + Path('Documents/Python/Projects/coinmaker/secrets')
     DEFAULT_SECRETS_FILEPATH = 'C:\\Users\\MikeAdmin\\Documents\\Python\\Projects\\coinmaker\\secrets'
     DEFAULT_CONFIG_FILEPATH = 'C:\\Users\\MikeAdmin\\Documents\\Python\\Projects\\coinmaker\\etc\\coinmaker.conf'
     DEFAULT_DB_FILEPATH = 'C:\\Users\\MikeAdmin\\Documents\\Python\\Projects\\coinmaker\\var\\sqlite3db'
     DEFAULT_LOG_FILEPATH = 'C:\\Users\\MikeAdmin\\Documents\\Python\\Projects\\coinmaker\\logs\\coinmaker.log'
 elif os.name == 'posix':
-    DEFAULT_SECRETS_FILEPATH = '/home/mike/.dcabot_secrets'
-    DEFAULT_CONFIG_FILEPATH = '/home/mike/bin/coinmaker/coinmaker/etc/coinmaker.conf'
+    install_dir = '/opt/coinmaker/'
+    config_dir = '/etc/opt/coinmaker/'
+    secrets_dir = homedir + '.dcabot_secrets'
+    DEFAULT_SECRETS_FILEPATH = homedir + '.dcabot_secrets'
+    DEFAULT_CONFIG_FILEPATH = '/etc/opt/coinmaker/'
+    #DEFAULT_CONFIG_FILEPATH = '/home/mike/bin/coinmaker/coinmaker/etc/coinmaker.conf'
     DEFAULT_DB_FILEPATH = '/home/mike/bin/coinmaker/coinmaker/var/sqlitedb'
     DEFAULT_LOG_FILEPATH = '/home/mike/bin/coinmaker/coinmaker/logs/coinmaker.log'
     pass
 
 else:
-    print(f'OS "{os.name}" not supported!')
+    log.critical(f"OS '{os.name}' not supported! (Supported versions are 'nt' or 'posix')")
     exit(1)
+
+def check_config():
+    pass
+
+if __name__ == '__main__':
+    logging.info(f"{__name__}")
+    check_config()
--- cm_secrets.py
+++ cm_secrets.py
@@ -1,30 +1,30 @@
 import configparser
 import os.path
 from cbpro import AuthenticatedClient
-from CONSTANTS import *
+import CONSTANTS as C
 ########
 # This file should be used by any other file that needs to import secret variables. 
 # In otherwords, do not read secrets from any file but this one!
 # I guess you can use the class vars to get secrets... maybe there's a way around this.
 ########
 
-def get_coinbase_credentials(cb_profile_name='coinbase_sandbox'):
+def get_coinbase_credentials(profile='coinbase_sandbox'):
     d = {}
-    config = configparser.ConfigParser()
-    config.read(DEFAULT_SECRETS_FILEPATH)
-    d['key']        = config[cb_profile_name]['api_key']
-    d['b64secret']  = config[cb_profile_name]['api_secret']
-    d['passphrase'] = config[cb_profile_name]['api_password']
-    d['api_url']    = config[cb_profile_name]['api_url']
+    secrets= configparser.ConfigParser()
+    secrets.read(C.DEFAULT_SECRETS_FILEPATH)
+    d['key']        = secrets[profile]['api_key']
+    d['b64secret']  = secrets[profile]['api_secret']
+    d['passphrase'] = secrets[profile]['api_password']
+    d['api_url']    = secrets[profile]['api_url']
     return d
 
 def get_influxdb_credentials():
     creds = {}
-    config = configparser.ConfigParser()
-    config.read(DEFAULT_SECRETS_FILEPATH)
-    creds['host']       = config['influxdb']['host']
-    creds['port']       = config['influxdb']['port']
-    creds['username']   = config['influxdb']['username']
-    creds['password']   = config['influxdb']['password']
-    # creds['database']         = config['influxdb']['database']
+    secrets= configparser.ConfigParser()
+    secrets.read(C.DEFAULT_SECRETS_FILEPATH)
+    creds['host']       = secrets['influxdb']['host']
+    creds['port']       = secrets['influxdb']['port']
+    creds['username']   = secrets['influxdb']['username']
+    creds['password']   = secrets['influxdb']['password']
+    # creds['database']         = secrets['influxdb']['database']
     return creds
